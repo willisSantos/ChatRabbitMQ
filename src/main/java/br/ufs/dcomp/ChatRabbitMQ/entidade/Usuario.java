e2f75af3d818bcccc7e4b8f5ed78d0a696950a2c
willisSantos <39805208+willisSantos@users.noreply.github.com>
2021-01-31 14:39:18 -0300

    Etapa1 (#1)
    * gitignore adicionado
    
    * finalização da etapa 1
    
    Co-authored-by: willis <willis@getinfo.net.br>

---
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/entidade/Usuario.java
@@ -0,0 +1,13 @@
+package br.ufs.dcomp.ChatRabbitMQ.entidade;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Usuario {
+
+	private String nome;
+}
